a38247aa9f3fb3584fd7f701a2645715c755644d
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-08-01 20:32:44 +0530

    fix the issue in form

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,10 +1,15 @@
-const InputField = ({name}) =>{
+const InputField = ({name, formInfo}) =>{
     return <label>
         <span>{name}</span>
         {
             name === 'Name' || name === 'Email' ?
-            <input type={name === 'Name' ? 'text' : 'email'} name={name.toLowerCase()} placeholder={`Enter Your ${name}`} /> : 
-            <textarea placeholder={`Enter Your ${name}`} name={name.toLowerCase()}></textarea>
+            <input 
+                type={name === 'Name' ? 'text' : 'email'} 
+                value={name === 'Name' ? formInfo.name : formInfo.email} 
+                name={name.toLowerCase()} 
+                placeholder={`Enter Your ${name}`} 
+            /> : 
+            <textarea placeholder={`Enter Your ${name}`} value={formInfo.message} name={name.toLowerCase()}></textarea>
         }
     </label>
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,15 +3,27 @@ import InputField from '../components/InputField';
 
 import {contactTitleDesc} from '../data';
 
+const initialState = {
+    Name: '',
+    Email: '',
+    Message: ''
+}
+
 const Contact = () =>{
+    const [formInfo, setFormInfo] = useState(initialState);
+    const handleForm = event =>{
+        event.preventDefault();
+        const {name, value} = event.target;
+        setFormInfo(prev => ({...prev, [name]: value}));
+    }
     return(
         <div className="contact" id="contact">
             <div className="common-section">
                 <TitleAndDesc {...contactTitleDesc} />
                 <div className="contact-section">
-                    <form action="https://formsubmit.co/fe4d0c6292724aab0b454b445d67175a" method="POST">
+                    <form action="https://formsubmit.co/fe4d0c6292724aab0b454b445d67175a" method="POST" onSubmit={handleForm}>
                         {
-                            ['Name', 'Email', 'Message'].map(name => <InputField key={name} name={name} />)
+                            ['Name', 'Email', 'Message'].map(name => <InputField key={name} name={name} formInfo={formInfo} />)
                         }
                         <button type="submit">Send</button>
                     </form>
