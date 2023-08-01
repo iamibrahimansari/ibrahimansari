4ebcd7b6580ad64288b6468e6e67b5f9d225515d
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-08-01 20:09:31 +0530

    replace my email id with string

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () =>{
             <div className="common-section">
                 <TitleAndDesc {...contactTitleDesc} />
                 <div className="contact-section">
-                    <form action="https://formsubmit.co/iamibrahimansari@gmail.com" method="POST">
+                    <form action="https://formsubmit.co/fe4d0c6292724aab0b454b445d67175a" method="POST">
                         {
                             ['Name', 'Email', 'Message'].map(name => <InputField key={name} name={name} />)
                         }
