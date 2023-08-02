ac93a5e176b6eb53656916fe4d1814ea4010ac17
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-08-02 11:23:23 +0530

    remove unwanted text

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -11,9 +11,6 @@ const Intro = () =>{
                 <SocialLinks />
                 <TitleAndDesc {...introTitleDesc} />
                 <Button prop="PROJECTS" />
-                <h2 style={{textAlign: 'center', marginTop: '3rem', fontSize: '3rem', color: 'red'}}>
-                    Site is in working mode. Available now for big screen such as Laptop, Desktop and T.V. It will be available soon for all devices.
-                </h2>
             </div>
         </div>
     )
